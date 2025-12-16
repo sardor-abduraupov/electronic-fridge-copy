81ddae10801f5d7efb74b28fcbbdb62b956206bd
Sardor Abduraupov <abduraupov.s.r@gmail.com>
2025-12-16 09:10:07 +0500

    yes1

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   // When deploying to GitHub Pages for a repo site, set the base to
   // '/<repo-name>/' so built assets use the correct path. Adjust the
   // string below if your repo name is different.
-  base: '/electronic-fridge-copy/',
+  base: '/',
   plugins: [react()],
   server: {
     host: true, // Needed for network access (e.g. 192.168.x.x)
