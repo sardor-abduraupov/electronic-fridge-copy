28052f8a8c05bd18a1fb1e52647d6642c3c8c9fc
Sardor Abduraupov <abduraupov.s.r@gmail.com>
2025-12-17 06:40:18 +0500

    new 21

--- index.tsx
+++ index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Polyfill process for browser environment to prevent "process is not defined" errors
-if (typeof process === 'undefined') {
-  (window as any).process = { env: {} };
+// Polyfill process for browser environment (safe for TypeScript)
+if (!(globalThis as any).process) {
+  (globalThis as any).process = { env: {} };
 }
 
 const rootElement = document.getElementById('root');
